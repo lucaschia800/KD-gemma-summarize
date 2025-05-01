c1b77831de1eb99229d196f7ad6c91c31610dccc
lucaschia800 <lucaschia800@gmail.com>
2025-04-30 18:10:07 -0700

    token set up

--- start.py
+++ start.py
@@ -1,14 +1,22 @@
 import os
 import torch
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+from huggingface_hub import login
 from datasets import load_dataset, concatenate_datasets
 from trl import SFTTrainer
 from transformers import TrainingArguments
 
+huggingface_token = os.environ.get('HUGGINGFACE_HUB_TOKEN')
+if huggingface_token is None:
+    raise ValueError("Please set the HUGGINGFACE_HUB_TOKEN environment variable.")
+os.environ["HUGGINGFACE_HUB_TOKEN"] = huggingface_token
+login(token=huggingface_token)
+
 
 teacher_name = "mistralai/Mistral-7b-v0.2"
 teacher = AutoModelForCausalLM.from_pretrained(
-        teacher_name, torch_dtype=torch.float16, device_map="auto"
+        teacher_name, torch_dtype=torch.float16, device_map="auto",
+        token = huggingface_token
     )
 
 tokenizer = AutoTokenizer.from_pretrained(teacher_name)
