8f77327030ed55374e2b987a1d6340cdf9b65e45
lucaschia800 <lucaschia800@gmail.com>
2025-05-04 00:28:41 -0700

    debug

--- tokenize_data.py
+++ tokenize_data.py
@@ -74,8 +74,8 @@ print(raw_ds[0])
 proc_ds = raw_ds.map(
     build_example,
     num_proc=16,          
-    remove_columns=raw_ds.column_names,  # drop old columns, keep the new ones
-    batch
+    remove_columns=raw_ds.column_names  # drop old columns, keep the new ones
+
 )
 
 print(proc_ds[0])
