ee50c7302db76d46d12ffc20e773d6ddfafec7df
lucaschia800 <lucaschia800@gmail.com>
2025-05-04 23:34:05 -0700

    eval script

--- evaluate_sum.py
+++ evaluate_sum.py
@@ -1,12 +1,15 @@
-import evaluate, transformers, torch
-from datasets import load_dataset
+import transformers, torch
+from datasets import load_dataset, load_from_disk
 import os
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from huggingface_hub import login
+from evaluate import evaluator
+import json
 
 huggingface_token = os.environ.get('HUGGINGFACE_HUB_TOKEN')
 if huggingface_token is None:
     raise ValueError("Please set the HUGGINGFACE_HUB_TOKEN environment variable.")
+
 login(token=huggingface_token)
 
 
@@ -17,4 +20,26 @@ teacher = AutoModelForCausalLM.from_pretrained(
     )
 
 tokenizer = AutoTokenizer.from_pretrained(teacher_name)
+test_ds = load_from_disk("mistral-KD/data/cnn_eval")
+
+
+task_evaluator = evaluator("summarization")
+
+results = task_evaluator.compute(
+    model = teacher,
+    metric = "rouge",
+    tokenizer = tokenizer,
+    data = test_ds,
+    strategy = "bootstrap",
+    n_resamples = 250,
+    label_colum = "summary",
+    input_column = "article"
+)
+
+
+print(results)
+
+with open("eval_results/teacher_rouge_results.json", "w") as f:
+    json.dump(results, f, indent=4)
+
 
